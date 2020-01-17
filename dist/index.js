ae086c7c4eef414d4280e5a99aa8b78b5e85efe0
JBU-seol <seolki100480@gmail.com>
2020-01-17 14:50:01 +0900

    typescript #1

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return 'hello ' + name + ' you are a ' + age + ' and ' + gender;
+};
+console.log(sayHi("Lee", 25, "male"));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name:string, age:number, gender:string):string => {
+    return 'hello '+name+' you are a '+age +' and '+gender;
+};
+
+console.log(sayHi("Lee",25,"male"));
+
+export {};
